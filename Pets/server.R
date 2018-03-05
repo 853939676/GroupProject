f60a4369e41748d40eb6399bb1081dbf1434d4eb
Jayna <jaynawang@Jaynas-MacBook-Air-2.local>
2018-03-04 16:30:56 -0800

    add select box code

--- Pets/server.R
+++ Pets/server.R
@@ -1,26 +1,20 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
+  output$value <- renderPrint({  # change renderPrint to renderPlot
+    if(input$select == 1) {
+      print("this should plot by gender")
+      # add in plot function for GENDER
+      
+    } else if(input$select == 2) {
+      print("this should plot by breed")
+      # add in plot function for BREED
+      
+    } else {
+      print("")
+      # plot something random or first plot (because it will prob never get used)
+    }
+      
+      })
 })
