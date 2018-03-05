b84dfe552f2402d3e723c32953e4de7211712eaa
853939676 <liurui@uw.edu>
2018-03-04 22:00:25 -0800

    finished publish

--- Pets/server.R
+++ Pets/server.R
@@ -7,9 +7,14 @@ source('./function.R')
 
 # Read dataset
 pets <- read.csv("Lost__found__adoptable_pets.csv", stringsAsFactors = FALSE, sep = ",", header = TRUE)
-
+pets <- select(pets, impound_no, Animal_ID, Record_Type, Current_Location, Animal_Name, animal_type, Age, Animal_Gender,
+               Animal_Breed, Animal_Color, Date, City, State, Zip)
 # Define server logic required to draw a histogram/donut
 shinyServer(function(input, output) {
+  #diaplay the whole dataframe
+  output$Table <- DT::renderDataTable({
+    DT::datatable(pets , options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+  })
   
   #creat Donut chart
   output$Donut <- renderPlotly({
@@ -19,10 +24,6 @@ shinyServer(function(input, output) {
   # create bar chart
   output$Bar <- renderPlotly({
     return(plot(pets, "bar", input$checkGroup, input$status, input$select))
-  })
-  
-  output$Table <- DT::renderDataTable({
-    DT::datatable(pets, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
   })
   
 })
--- Pets/ui.R
+++ Pets/ui.R
@@ -1,34 +1,53 @@
 library(shiny)
 library(plotly)
 library(ggplot2)
+library(markdown)
 
-shinyUI(fluidPage(sidebarLayout(
-  sidebarPanel(
-    
-    ## create checkbox menu for dog cat etc
-    checkboxGroupInput("checkGroup", label = h3("Check One or More Type of Pets"), 
-                       choices = list("Dog" = "Dog", "Cat" = "Cat", "Other" = "Other"),
-                       selected = "Dog"
-                       ),
-    
-    selectInput("status", label = h3("Select Status of Pets"),
-                choices = list("Adoptable" = "A", "Lost" = "L", "Found" = "F"),
-                selected = "A"
-                ),
-    
-    selectInput("select", label = h3("Display plot by:"),
-                choices = list("Gender" = "G", "Breed" = "B")
-                )
-  ),
+# Create Navbar Page
+# About, Table, Plot
+navbarPage("Welcome!",
+  tabPanel("About",
+           fluidRow(column(6, includeMarkdown("about.md"))
+                    )
+           ),
   
-  # Show a plot of the generated distribution
-  mainPanel(# Create tabs for page
-    tabsetPanel(
-      type = "tabs",
-      tabPanel("Donut Chart", plotlyOutput("Donut")),
-      tabPanel("Bar Chart", plotlyOutput("Bar")),
-      tabPanel("Table", DT::dataTableOutput("Table"))
+  tabPanel("Table",
+           DT::dataTableOutput("Table")
+           ),
+  
+  tabPanel("Plot",
+    (sidebarLayout(
+      sidebarPanel(
+        ## create checkbox menu for dog cat etc
+        checkboxGroupInput("checkGroup", label = h3("Check One or More Type of Pets"),
+                           choices = list("Dog" = "Dog", "Cat" = "Cat", "Other" = "Other"),
+                           selected = "Dog"
+                           ),
+        
+        selectInput("status", label = h3("Select Status of Pets"), 
+                    choices = list("Adoptable" = "A", "Lost" = "L", "Found" = "F"),
+                    selected = "A"
+                    ),
+        
+        selectInput("select", label = h3("Display plot by:"),
+                    choices = list("Gender" = "G", "Breed" = "B")
+                    )
+        ),
       
-      
-    ))
-)))
+      # Show a plot of the generated distribution
+      mainPanel(# Create tabs for page
+        tabsetPanel(
+          type = "tabs",
+          tabPanel("Donut Chart", plotlyOutput("Donut")),
+          tabPanel("Bar Chart", plotlyOutput("Bar"))
+          )
+        )
+      )
+     )
+  )
+)
+
+    
+    
+    
+    
