716db0509aed68668f42a809adead10bd9a886e3
853939676 <liurui@uw.edu>
2018-03-05 19:28:12 -0800

    update DT

--- Pets/server.R
+++ Pets/server.R
@@ -12,10 +12,10 @@ pets <- select(pets, impound_no, Animal_ID, Record_Type, Current_Location, Anima
                Animal_Breed, Animal_Color, Date, City, State, Zip)
 # Define server logic required to draw a histogram/donut
 shinyServer(function(input, output) {
-  #diaplay the whole dataframe
-  # output$Table <- DT::renderDataTable({
-  #   DT::datatable(pets , options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
-  # })
+  # diaplay the whole dataframe
+   output$Table <- DT::renderDataTable({
+     DT::datatable(pets , options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+   })
   
   #creat Donut chart
   output$Donut <- renderPlotly({
